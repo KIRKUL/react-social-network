13f5ad5cfad9a1cd17b8f8a22a6b963deab0cccf
Kirill Kulev <kirkulev101@mail.ru>
2021-08-21 17:28:17 +0300

    add component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://im0-tub-ru.yandex.net/i?id=b505aae8ec6bdd15b2b01d94a901f635&n=13'></img>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+    <div>
+      <img src='https://drasler.ru/wp-content/uploads/2019/05/%D0%9E%D0%B1%D0%BE%D0%B8-%D0%B4%D0%BB%D1%8F-%D1%80%D0%B0%D0%B1%D0%BE%D1%87%D0%B5%D0%B3%D0%BE-%D1%81%D1%82%D0%BE%D0%BB%D0%B0-%D0%BC%D0%BE%D1%80%D0%B5-%D0%BF%D0%BB%D1%8F%D0%B6-%D0%B8-%D0%BF%D0%B0%D0%BB%D1%8C%D0%BC%D1%8B020.jpg'></img>
+    </div>
+    <div>
+      ava
+    </div>
+    <div>
+      my post
+      <div>
+        new post
+      </div>
+      <div>
+        <div>
+          post1
+        </div>
+        <div>
+          post 2
+        </div>
+      </div>
+    </div>
+  </div>
+}
+export default Profile;
