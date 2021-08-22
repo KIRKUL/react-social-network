2c94257954785537e613d76a4ae07913232e157a
Kirill Kulev <kirkulev101@mail.ru>
2021-08-23 00:10:50 +0300

    add navbar

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
 import Navbar from './components/Navbar/Navbar';
 import Dialogs from './components/Dialogs/Dialogs';
-
-
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 const App = () => {
   return (
+    <BrowserRouter>
     <div className='app-wrapper'>
       <Header />
       <Navbar />
       <div class='app-wrapper-content'>
-        <Dialogs />
-      </div>
-      {/* <Profile /> */}
+        <Route path='/dialogs' component={Dialogs} />
+        <Route path='/profile' component={Profile} />
+        <Route path='/news' component={News} />
+        <Route path='/music' component={Music} />
+        <Route path='/settings' component={Settings} />
 
+      </div>
     </div>
+    </BrowserRouter>
+
   );
 }
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import s from './Navbar.module.css';
-
+import {NavLink} from "react-router-dom";
 // let s = {
 //   'nav': 'Navbar_nav_3ou9Q',
 //   'item': 'Navbar_item_3qaF3',
@@ -9,28 +9,30 @@ import s from './Navbar.module.css';
 
 let c1 = "item";
 let c2 = "active";
-let classes = c1 +""+c2;
+let classes = c1 + "" + c2;
 let classesNew = `${s.item} ${s.active}`;
 
 
 const Navbar = () => {
-    return <nav className={s.nav}>
-    <div className={s.item}>
-      <a>Profile</a>
-    </div>
-    <div className={`${s.item} ${s.active}`}>
-      <a>Messages</a>
-    </div>
-    <div className={s.item}>
-      <a>News</a>
-    </div>
-    <div className={s.item}>
-      <a>Music</a>
-    </div>
-    <div className={s.item}>
-      <a>Settings</a>
-    </div>
-  </nav>
+  return (
+    <nav className={s.nav}>
+      <div className={s.item}>
+        <NavLink to="/profile" activeClassName={s.active}>Profile</NavLink>
+      </div>
+      <div className={`${s.item} ${s.active}`}>
+        <NavLink to="/dialogs" activeClassName={s.active}>Messages</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/news" activeClassName={s.active}>News</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/music" activeClassName={s.active}>Music</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/settings" activeClassName={s.active}>Settings</NavLink>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar;
