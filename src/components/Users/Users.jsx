a14a3b4242b9acf20cc3fc08115e6db4b5d8a676
Kirill Kulev <kirkulev101@mail.ru>
2021-09-16 00:11:58 +0300

    add user profile

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,23 +1,22 @@
 import React from "react";
+import axios from "axios";
 import styles from "./users.module.css"
+import userPhoto from "../../../src/assets/images/user.png"
 
 let Users = (props) => {
     if (props.users.length ===0) {
-
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            debugger;
+            props.setUsers (response.data.items)
+        });
     
-    props.setUsers (  [
-        { id: 1, photoUrl:'https://img2.freepng.ru/20180625/ztq/kisspng-user-royalty-free-laptop-clip-art-office-suit-5b31ad8c60fa86.5587156515299823483972.jpg', followed: false, fullname: 'Dmitry', status: 'Iam a boss', location: {city:'Minsk', country: 'Belarus'} },
-        { id: 2, photoUrl:'https://e7.pngegg.com/pngimages/620/285/png-clipart-computer-icons-computer-operator-child-hand.png',followed: true, fullname: 'Alexander', status: 'Iam a boss too', location: {city:'Moscow', country: 'Russia'} },
-        { id: 3, photoUrl:'https://st3.depositphotos.com/1007566/13792/v/950/depositphotos_137920280-stock-illustration-businessman-working-in-computer.jpg',followed: false, fullname: 'Andrew', status: 'Iam a engineer', location: {city:'Kovrov', country: 'Russia'} }
 
-    ]
-    )
 }
     return <div>{
         props.users.map( u=> <div key= {u.id}> 
         <span>
             <div>
-                <img src={u.photoUrl} className={styles.userPhoto} />
+                <img src={u.photos.small !=null ? u.photos.small : userPhoto } className={styles.userPhoto} />
             </div>
             <div>
                 {u.followed 
@@ -31,9 +30,9 @@ let Users = (props) => {
             <div>{u.status}</div>
             </span>
             <span>
-            <div>{u.location.country}</div>
+            <div>{"u.location.country"}</div>
 
-                <div>{u.location.city}</div>
+                <div>{"u.location.city"}</div>
             </span>
         </span>
 
